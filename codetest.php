15b522fc8a5359299bec946c0f27a47df9c812bd
Jim Rohrer <jrohrer1@gmail.com>
2013-02-19 19:08:12 -0500

    Code test finished

---
+++ codetest.php
@@ -0,0 +1,31 @@
+<?php
+function sanitize_name($name) {
+	$name = trim($name);
+	$name = preg_replace('/[^a-zA-Z\s]/', "", $name);
+	$name = preg_replace('/[\s+]/', " ", $name);
+	return ucwords(strtolower($name));
+}
+
+function age_of($name) {
+	try {
+	  $conn = new PDO('mysql:host=localhost;dbname=company', "bob", "likeskittens");
+	  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $conn;
+	} catch(PDOException $e) {
+	  return $e;
+	}
+	
+	$stmt = $conn->prepare("SELECT age FROM person WHERE name = :name");
+	$stmt->bindParam(':name', $name);
+	$stmt->execute();
+	$obj = $stmt->fetchObject();
+	if(count($obj) > 0) {
+		return $obj->age;
+	} else {
+		return false;
+	}
+}
+
+echo sanitize_name("  i   &%$   am  #1   name");
+
+?>
